c6fe30ce5fdd11076ece2d8b1fb1c5aede962e06
remuslum <57741494+remuslum@users.noreply.github.com>
2025-08-01 19:04:08 +0800

    Completed animation

--- code/26 Animations/01-starting-project/src/components/Challenges.jsx
+++ code/26 Animations/01-starting-project/src/components/Challenges.jsx
@@ -1,4 +1,5 @@
 import { useContext, useState } from 'react';
+import { AnimatePresence, motion } from 'motion/react';
 
 import { ChallengesContext } from '../store/challenges-context.jsx';
 import ChallengeItem from './ChallengeItem.jsx';
@@ -40,8 +41,15 @@ export default function Challenges() {
         onSelectType={handleSelectType}
         selectedType={selectedType}
       >
+        <AnimatePresence mode="wait">
         {displayedChallenges.length > 0 && (
-          <ol className="challenge-items">
+          <motion.ol 
+          key="list" 
+          initial={{opacity: 0, y:-20}}
+          animate={{opacity: 1, y:0}}
+          exit={{y:-30,opacity:0}} 
+          className="challenge-items">
+            <AnimatePresence>
             {displayedChallenges.map((challenge) => (
               <ChallengeItem
                 key={challenge.id}
@@ -50,9 +58,16 @@ export default function Challenges() {
                 isExpanded={expanded === challenge.id}
               />
             ))}
-          </ol>
+            </AnimatePresence>
+          </motion.ol>
         )}
-        {displayedChallenges.length === 0 && <p>No challenges found.</p>}
+        {displayedChallenges.length === 0 && 
+          <motion.p 
+          key="fallback" 
+          initial={{opacity: 0, y: -20}}
+          animate={{opacity:1, y:0}}
+          exit={{opacity:0, y:-20}}>No challenges found.</motion.p>}
+        </AnimatePresence>
       </ChallengeTabs>
     </div>
   );
--- code/26 Animations/01-starting-project/src/components/Modal.jsx
+++ code/26 Animations/01-starting-project/src/components/Modal.jsx
@@ -1,13 +1,25 @@
 import { createPortal } from 'react-dom';
+import { motion } from 'motion/react';
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog 
+      open 
+      className="modal"
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      variants={{
+        hidden: {opacity: 0, y:30},
+        visible: {opacity: 1, y:0},
+        exit: {opacity: 0, y:30}
+      }}
+      >
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
